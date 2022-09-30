5fb5529e74076db450bf187663e92d89c3fd9c0c
Kyohei Sasaki <106052021+kyohei960@users.noreply.github.com>
2022-09-30 14:23:31 +0000

    [finish]route

--- config/routes.rb
+++ config/routes.rb
@@ -8,27 +8,44 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
-    scope module: :public do
-    root to: "homes#top"
-    get '/about' => 'homes#about', as: 'about'
-    
-    resources :items, only: [:index, :show]
-    
-    get '/mypage' => 'public#show'
-    get '/mypage' => 'public#edit'
-    patch '/mypage' => 'public#update'
-    get '/mypage/unsubscribe' => 'public#unsubscribe', as: 'unsubscribe'
-    patch '/mypage/withdraw' => 'public#withdraw', as: 'withdraw'
-    
-    delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
-    resources :cart_items, only: [:index, :update, :destroy, :create]
-    
-    post '/orders/confirmation' => 'orders#confirmation'
-    get '/orders/complete' => 'orders#complete'
-    resources :orders, only: [:index, :show, :new, :create]
-    
-    resources :addresses, only: [:index, :create, :edit, :update, :destroy]
-    
-    get "/items/genre_search/:id" => "items#genre_search", as: 'items_genre_search'
-    end
+
+  scope module: :public do
+  root to: "homes#top"
+  get '/about' => 'homes#about', as: 'about'
+
+  resources :items, only: [:index, :show]
+
+  get '/mypage' => 'public#show'
+  get '/mypage' => 'public#edit'
+  patch '/mypage' => 'public#update'
+  get '/mypage/unsubscribe' => 'public#unsubscribe', as: 'unsubscribe'
+  patch '/mypage/withdraw' => 'public#withdraw', as: 'withdraw'
+
+  delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
+  resources :cart_items, only: [:index, :update, :destroy, :create]
+
+  post '/orders/confirmation' => 'orders#confirmation'
+  get '/orders/complete' => 'orders#complete'
+  resources :orders, only: [:index, :show, :new, :create]
+
+  resources :addresses, only: [:index, :create, :edit, :update, :destroy]
+
+  get "/items/genre_search/:id" => "items#genre_search", as: 'items_genre_search'
+
+  end
+
+  namespace :admin do
+  root to: "homes#top"
+
+  resources :items, except: [:destroy]
+
+  resources :genres, only: [:index, :create, :edit, :update]
+
+  resources :customers, only: [:index, :show, :edit, :update]
+
+  resources :orders, only: [:show, :update]
+
+  resources :order_details, only: [:update]
+
+  end
 end
