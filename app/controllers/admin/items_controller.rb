66e4ac25216471dcaa6bba8749272dadbdb072e5
Kyohei Sasaki <106052021+kyohei960@users.noreply.github.com>
2022-10-24 13:03:52 +0000

    [add]adminコントローラ追記

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,24 +4,42 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
+    @genres = Genre.all
   end
 
   def new
     @item = Item.new
+    @genres = Genre.all
   end
-  
+
   def create
     @item = Item.new(admin_item_params)
     if @item.save
       redirect_to admin_item_path(@item.id)
     else
+      @genres = Genre.all
       render :new
     end
   end
-  
+
   def update
+    @item = Item.find(params[:id])
+    if @item.update(admin_item_params)
+      redirect_to admin_item_path(@item.id)
+    else
+      @genres = Genre.all
+      render :edit
+    end
+  end
+
+  private
+
+  def admin_item_params
+    params.require(:item).permit(:name, :image, :genre_id, :description, :price, :is_active)
   end
 end
